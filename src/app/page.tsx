c8cbe0bc9485a1277eb9f44ec906e6170bdd017c
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-12-25 15:54:54 +0700

    Fix copy to clipboard button to prevent default behavior on click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,10 @@ export default function Home() {
     <main className="w-screen h-screen flex-col justify-center">
       <div className="flex justify-center">
         <button
-          onClick={copyToClipboard}
+          onClick={() => {
+            copyToClipboard();
+            return false;
+          }}
           className="bg-primary-500 px-4 py-2 text-4xl"
         >
           Copy Link ! V.2
